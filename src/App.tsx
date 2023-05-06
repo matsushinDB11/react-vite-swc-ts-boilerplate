9468cc0bed86c0b6d011c1b00b3048944c7c9a50
Shintaro Matsumoto <development@shintaro-matsumoto.com>
2023-05-07 03:12:54 +0900

    :wip eslintの警告を修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
+import { useState, FC } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+const App:FC = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+          <img src="vite.svg" className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
