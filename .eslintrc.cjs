8354f4f87ddf3655ec200ce6cd70e41fd26c07c9
Shintaro Matsumoto <development@shintaro-matsumoto.com>
2023-05-07 01:28:50 +0900

    ダブルクオートに変更

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,18 +4,18 @@ module.exports = {
     es2021: true
   },
   extends: [
-    'plugin:react/recommended',
-    'standard-with-typescript',
-    'prettier'
+    "plugin:react/recommended",
+    "standard-with-typescript",
+    "prettier"
   ],
   overrides: [
   ],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
   plugins: [
-    'react'
+    "react"
   ],
   rules: {
   }
