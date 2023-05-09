d3a157cf41c9ecc57e702ca720f3db948e23f77b
Shintaro Matsumoto <development@shintaro-matsumoto.com>
2023-05-09 13:14:44 +0900

    fix: eslintの警告を修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState, FC } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState, type FC } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
-const App:FC = () => {
-  const [count, setCount] = useState(0)
+const App: FC = () => {
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,18 +17,20 @@ const App:FC = () => {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          onClick={() => {
+            setCount((count) => count + 1);
+          }}
+        >
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
